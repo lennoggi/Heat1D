bf976b7baec9e6c605164b2d2aac9114c8230de1
Lorenzo Ennoggi <lorenzo.ennoggi@gmail.com>
2026-04-02 16:25:36 +0200

    include/Check_parameters.hh : add 0 after decimal point

--- include/Check_parameters.hh
+++ include/Check_parameters.hh
@@ -6,16 +6,16 @@
 
 static_assert(INITIAL_DATA == GAUSSIAN);
 
-static_assert(ALPHA > 0.);
+static_assert(ALPHA > 0.0);
 
 static_assert(NX > 1);
-static_assert(L  > 0.);
+static_assert(L  > 0.0);
 
 static_assert(NT > 1, "Need at least two time steps for correct output handling");
-static_assert(DT > 0.);
+static_assert(DT > 0.0);
 
-static_assert(SIGMA > 0.);
-static_assert(X0 - 3.*SIGMA > 0. and X0 + 3*SIGMA < L - L/static_cast<double>(NX),
+static_assert(SIGMA > 0.0);
+static_assert(X0 - 3.0*SIGMA > 0.0 and X0 + 3.0*SIGMA < L - L/static_cast<double>(NX),
               "Initial function is quite out of bounds");
 
 static_assert(OUT_EVERY > 0);
